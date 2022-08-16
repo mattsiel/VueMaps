94980431492f245ce5e0ac8d455ad92a6f1df5bd
mattsii <msielecki2@gmail.com>
2022-08-16 07:43:04 -0700

    Update Map.vue

--- map-app/src/components/Map.vue
+++ map-app/src/components/Map.vue
@@ -6,39 +6,35 @@ import L from "leaflet";
 // https://gis.stackexchange.com/questions/402871/custom-maps-in-leaflet-with-maptiler-and-geojson
 
 export default {
-  name: "Vue-Map",
-  data() {
-    return {
-      center: [37, 7749, -122, 4194],
-    };
-  },
-  methods: {
-    setupLeafletMap: function () {
-        
-      const mapDiv = L.map("mapContainer");
-      L.tileLayer(
-        "../../public/spain.geojson",
-        {
-          maxZoom: 18,
-        }
-      ).addTo(mapDiv);
-    },
-  },
-  mounted() {
-    this.setupLeafletMap();
-  },
+ name: "VueMap",
+ data() {
+   return{
+     center: [37,7749, -122,4194]
+   }},
+ methods: {
+   setupLeafletMap: function () {
+     const mapDiv = L.map("mapContainer").setView(this.center, 13);
+     L.tileLayer(
+       "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+       {
+         attribution:
+           'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+         maxZoom: 18,
+         id: "mapbox/streets-v11",
+         accessToken:"XXX"
+       }
+     ).addTo(mapDiv);
+   },
+ },
+ mounted() {
+   this.setupLeafletMap();
+ },
 };
 </script>
 
-<template>
-  <div id="container">
-    <div id="mapContainer"></div>
-  </div>
-</template>
-
 <style scoped>
-    #mapContainer {
-    width: 80vw;
-    height: 100vh;
-    }
+#mapContainer {
+ width: 80vw;
+ height: 100vh;
+}
 </style>
